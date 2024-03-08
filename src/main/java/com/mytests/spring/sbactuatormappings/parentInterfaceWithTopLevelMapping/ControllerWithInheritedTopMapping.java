5d62fa8902d7daa0d5edb37426c6efd3c5478717
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2024-03-08 09:01:07 +0100

    comments

--- src/main/java/com/mytests/spring/sbactuatormappings/parentInterfaceWithTopLevelMapping/ControllerWithInheritedTopMapping.java
+++ src/main/java/com/mytests/spring/sbactuatormappings/parentInterfaceWithTopLevelMapping/ControllerWithInheritedTopMapping.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControllerWithInheritedTopMapping implements ControllerWithInheritedTopMappingDefinition {
 
     // expected: endpoints are shown as /toplevel/test1, /toplevel/test2
-    // actual: they are shown as /test1 and /test2
+    // actual: they are shown as /test1 and /test2  - fixed
+    // generated openAPI draft doesn't consider the parent class mapping - https://youtrack.jetbrains.com/issue/IDEA-348580
     @GetMapping("/test1")
     public String test1() {
         return "toplevel test1";
